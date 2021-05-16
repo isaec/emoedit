f3cf2630df80fcfa7e5d45b8efd1880556c2a84e
isaac <isaac@rubenfamily.com>
2021-05-15 21:29:46 -0700

    add settings page

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import { commands } from "@goosemod/patcher"
+import { createItem, removeItem } from '@goosemod/settings'
+
+const version = "0.0.1"
+
+const clearHistory = () => {
+    console.log("clear")
+}
 
 export default {
     goosemodHandlers: {
@@ -6,12 +13,36 @@ export default {
             commands.add(
                 "clearEmojiHistory",
                 "clear emoji history",
-                () => { console.log("clear!") },
+                clearHistory,
                 []
+            )
+        },
+        onLoadingFinished: () => {
+            createItem(
+                "emoedit",
+                [
+                    version,
+                    {
+                        type: "header",
+                        text: "editing"
+                    },
+                    {
+                        type: "text-and-danger-button",
+                        text: "clear all emoji use history",
+                        subtext: "all emoji history, including default suggestions will be erased",
+                        buttonText: "clear",
+                        onclick: clearHistory
+                    },
+                    {
+                        type: "header",
+                        text: "config"
+                    }
+                ]
             )
         },
         onRemove: () => {
             commands.remove("clearEmojiHistory")
+            removeItem("emoedit") //clean up, remove the settings pane
         }
     }
 }
